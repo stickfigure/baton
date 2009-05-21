e25413c58db4edd6c3ce5234b1de6ab09578f375
scotthernandez <scotthernandez@cfe93f62-4575-11de-89ec-3d9467717a5a>
2009-05-21 00:48:34 +0000

    working soon?

--- dotnet/src/BatonService.cs
+++ dotnet/src/BatonService.cs
@@ -1,9 +1,9 @@
 using System;
-
+using System.ServiceProcess;
 
 namespace Baton {
 
-	class BatonService {
+	class BatonService : ServiceBase {
 		
 		public static void Main() {
 			// Add your code here
